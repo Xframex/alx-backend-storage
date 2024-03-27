62e759eea5da3f34626364b5d202a88fdda1b199
Your Name <you@example.com>
2024-03-26 19:31:21 -0700

    task 12

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+'''Task 12
+'''
+from pymongo import MongoClient
+
+
+client = MongoClient()
+db = client.logs
+collection = db.nginx
+
+total_logs = collection.count_documents({})
+
+http_methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+method_counts = {method: collection.count_documents({"method": method}) for method in http_methods}
+
+status_check_count = collection.count_documents({"method": "GET", "path": "/status"})
+
+print(f"{total_logs} logs")
+print("Methods:")
+for method, count in method_counts.items():
+    print(f"    method {method}: {count}")
+print(f"{status_check_count} status check")
