f06af8b3d9c40b16aace63e4d654177285dda7c9
Your Name <you@example.com>
2024-03-26 19:40:20 -0700

    task12 fixxx

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,24 +1,25 @@
 #!/usr/bin/env python3
-'''Task 12
-'''
 from pymongo import MongoClient
 
-client = MongoClient()
-db = client.logs
-collection = db.nginx
 
-total_logs = collection.count_documents({})
+def print_nginx_request_logs(nginx_collection):
+    total_logs = nginx_collection.count_documents({})
+    print(f"{total_logs} logs")
+    
+    print("Methods:")
+    methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+    for method in methods:
+        req_count = nginx_collection.count_documents({'method': method})
+        print(f"\tmethod {method}: {req_count}")
 
-http_methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-method_counts = {
-    method: collection.count_documents({"method": method})
-    for method in http_methods
-}
+    status_checks_count = nginx_collection.count_documents({'method': 'GET', 'path': '/status'})
+    print(f"{status_checks_count} status check")
 
-status_check_count = collection.count_documents({"method": "GET", "path": "/status"})
 
-print(f"{total_logs} logs")
-print("Methods:")
-for method, count in method_counts.items():
-    print(f"    method {method}: {count}")
-print(f"{status_check_count} status check")
+def run():
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    print_nginx_request_logs(client.logs.nginx)
+
+
+if __name__ == '__main__':
+    run()
